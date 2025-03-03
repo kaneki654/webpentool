09c38cee16b854c7f6c995df371ba0412a35b3d5
kaneki654 <84548004+kaneki654@users.noreply.github.com>
2025-03-03 16:21:07 +0800

    Update pipinstall.py

--- pipinstall.py
+++ pipinstall.py
@@ -3,19 +3,19 @@ import os
 import requests
 
 def make():
-  os.system('tqdm')
-  os.system('beautifulsoup4')
-  os.system('httpx')
-  os.system('fake_useragent')
-  os.system('requests')
-  os.system('colorama')
-  os.system('sys')
-  os.system('socket')
-  os.system('re')
-  os.system('asyncio')
-  os.system('threading')
-  os.system('concurrent')
-  os.system('datetime')
+  os.system('pip install tqdm')
+  os.system('pip install beautifulsoup4')
+  os.system('pip install httpx')
+  os.system('pip install fake_useragent')
+  os.system('pip install requests')
+  os.system('pip install colorama')
+  os.system('pip install sys')
+  os.system('pip install socket')
+  os.system('pip install re')
+  os.system('pip install asyncio')
+  os.system('pip install threading')
+  os.system('pip install concurrent')
+  os.system('pip install datetime')
 
 if __name__ == "__main__":
   make()
