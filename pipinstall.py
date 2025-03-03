d730bdb26c80651b1a41a0a16acbed9cde16b0ef
kaneki654 <84548004+kaneki654@users.noreply.github.com>
2025-03-03 16:18:20 +0800

    Create pipinstall.py

---
+++ pipinstall.py
@@ -0,0 +1,18 @@
+import time
+import os
+import requests
+
+def make():
+  os.system('tqdm')
+  os.system('beautifulsoup4')
+  os.system('httpx')
+  os.system('fake_useragent')
+  os.system('requests')
+  os.system('colorama')
+  os.system('sys')
+  os.system('socket')
+  os.system('re')
+  os.system('asyncio')
+  os.system('threading')
+  os.system('concurrent')
+  os.system('datetime')
